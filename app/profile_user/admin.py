0bb95ef03038b22bc2c1ea0f4404c59cacc08f4b
proyectos <proyectos@ingytal.com>
2022-01-29 08:53:12 -0500

    Registro completo

--- app/profile_user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from profile_user.models import UserProfile
-
-class myUserProfileAdmin(admin.ModelAdmin):
-    list_display = ('username','email','telefono','id_cliente')
-
-admin.site.register(UserProfile,myUserProfileAdmin)
