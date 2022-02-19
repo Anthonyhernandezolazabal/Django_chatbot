e735e8726adf49144c20545f1df3ea16d5d445fd
proyectos <proyectos@ingytal.com>
2022-02-19 11:59:27 -0500

    cambio en el modelo chat_user en respuesta

--- app/chatbot/models.py
+++ app/chatbot/models.py
@@ -4,7 +4,7 @@ from chatbot_admin.models import cliente
 
 class chat_user(models.Model):
   pregunta = models.CharField(max_length=100,verbose_name='Escribir mensaje', null=True)
-  respuesta = models.CharField(max_length=255,verbose_name='Respuesta', default=None, null=True)
+  respuesta = models.TextField(verbose_name='Respuesta', default=None, null=True)
   key_session_alias = models.CharField(max_length=255,verbose_name='Alias usuario', default=None, null=True)
   nombre_persona = models.CharField(max_length=50,verbose_name='Nombre de la persona', null=True)
   cliente_empresa_id = models.ForeignKey(cliente,on_delete=models.SET_NULL, blank=True, null=True)
