0f0b61d936da1efb4ae8fd9d87accfd61b5251dd
proyectos <proyectos@ingytal.com>
2022-03-18 09:32:09 -0500

    modulo respuestas agregando .json

--- app/static/chatbot_admin/assets/js/respuestas.js
+++ app/static/chatbot_admin/assets/js/respuestas.js
@@ -1,4 +1,5 @@
 var url_servidor = 'chatbot.demoregistro.xyz';
+
 conversaciones_pyr()
 var Ls_rpt = localStorage.getItem('datos')
 var id_cliente_id = JSON.parse(Ls_rpt).id_cliente;
@@ -9,6 +10,7 @@ var id_empresa_id = JSON.parse(Ls_rpt).id_empresa;
 let btn_quest = document.querySelector("#add_quest");
 var cont = 0
 var micont = 0
+var contsl = 1;
 btn_quest.addEventListener("click", () => {
   cont++
   let contenedor = document.querySelector('#add_input_new')
@@ -62,24 +64,164 @@ function quitar_prg(btn) {
   localStorage.setItem("pregunta", JSON.stringify(prg))
 }
 /*== TIPO DE RESPUESTA ==*/
+/*===========================
+  TEXTO
+=============================*/
 let check_inpt3 = document.querySelector("#customRadio3");
 check_inpt3.addEventListener("click", () => {
-  var x = document.getElementById("add_chek_txt");
-  var y = document.getElementById("alert_check");
-  let titulo = document.getElementById('rptatit')
-  x.style.display = "block";
-  y.style.display = "none";
-  titulo.style.display = "block";
+  contsl = 1;
+  console.log('customRadio333 :', contsl)
+  var textarearpta = `
+                    <textarea class="form-control" placeholder="Ingresa aquí la autorespuesta" id="txt_autorpta"  name="txt_autorpta" style="height: 100px"></textarea>
+                    <label id='rmlbl01' for="txt_autorpta">Autorespuesta</label>`;
+  document.querySelector('#add_chek_txt').innerHTML = textarearpta;
+  document.getElementById('rptatit').style.display = "block";
+  document.getElementById('add_slider').style.display = "none";
+
+  document.querySelector('.cls_modal').classList.remove("modal-full-width")
+  // document.getElementById("divSlider").removeChild(document.getElementById("accordionExample"));
+  document.getElementById("divSlider").removeChild(document.getElementById("accordionExample"))
+  document.querySelector('#cls_col').className = 'col-lg-10';
 })
+/*===========================
+  SLIDER
+=============================*/
 let check_inpt4 = document.querySelector("#customRadio4");
 check_inpt4.addEventListener("click", () => {
-  var x = document.getElementById("add_chek_txt");
-  var y = document.getElementById("alert_check");
-  let titulo = document.getElementById('rptatit')
-  x.style.display = "none";
-  y.style.display = "block";
-  titulo.style.display = "none";
+
+  document.getElementById('add_slider').style.display = "contents";
+  console.log("slider :", document.getElementById('add_slider'))
+  var sliderHTML = `<div class="accordion" id="accordionExample">
+                      <div id='sld01'>
+                        <div class="card mb-0 clsSlider">
+                          <div class="card-header" id="heading01">
+                            <h5 class="m-0">
+                              <a class="custom-accordion-title d-block pt-2 pb-2" data-bs-toggle="collapse"
+                                href="#collapse01" aria-expanded="true" aria-controls="collapse01">
+                                Imágen de Slider #1 <i class="mdi mdi-close-circle-outline" style="margin-left: 15px;font-size: 20px;color: red;"></i>
+                              </a>
+                            </h5>
+                          </div>
+                          <div id="collapse01" class="collapse show" aria-labelledby="heading01"
+                            data-bs-parent="#accordionExample">
+                            <div class="card-body">
+                              <div class="mb-3">
+                                <div class="row mb-1">
+                                  <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                  <div class="col-md-10 col-lg-10 col-xl-10 col-xxl-10 col-sm-12">
+                                    <label for="example-fileinput" class="form-label">Cargar Archivo:</label>
+                                    <input type="file" id="example-fileinput01" class="form-control imagen">
+                                    <h6 class="text-warning">Puedes cargar: Imagen | Video | Audio</h6>
+                                  </div>
+                                  <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                </div>
+                                <div class="row">
+                                  <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                  <div class="col-md-10 col-lg-10 col-xl-10 col-xxl-10 col-sm-12">
+                                    <label for="example-fileinput" class="form-label">Descripción:</label>
+                                    <input type="text" class="form-control mb-2 titulo" id="validationTitulo01" placeholder="Título"
+                                      required>
+                                    <input type="text" class="form-control descripcion" id="validationDescrip01" placeholder="Descripción"
+                                      required>
+                                    <br>
+                                    
+                                    <label for="example-fileinput" class="form-label">Accion:</label>
+
+
+                                    <div class="mb-2 mt-2" id='add_input_new_action1'>
+                                    
+                                        <input class="form-control accioninicial" type="text" placeholder="Formular pregunta" id="textaction01" name="textaction01" style='margin-bottom: 15px;'>
+
+                                    </div>
+
+                                    <a style="cursor: pointer" onclick='add_new_accion(this)' attr_cont_total='1' class="text-primary btn_save_action"><i class="mdi mdi-plus-circle me-1"></i>Agregar otra acción</a>
+                                  
+                                  </div>
+                                  <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>`;
+  document.querySelector('#divSlider').innerHTML = sliderHTML;
+  document.querySelector('.cls_modal').className += " modal-full-width"; //Extiendo el modal agregando una clase
+  document.querySelector('#cls_col').className = 'col-lg-6';
+  document.getElementById('rptatit').style.display = "none";
+  document.getElementById("add_chek_txt").removeChild(document.getElementById("txt_autorpta"));
+  document.getElementById("add_chek_txt").removeChild(document.getElementById("rmlbl01"));
 })
+/*===========================
+  AGREGAR NUEVA IMAGEN A SLIDER
+=============================*/
+document.querySelector('#add_slider').addEventListener('click', function () {
+  contsl++
+  console.log('clickclick :', contsl)
+  let contenedorsl = document.querySelector('#accordionExample')
+  let sl = document.createElement('div')
+  sl.id = "sld0" + contsl;
+  sl.innerHTML = `
+                      <div class="card mb-0 clsSlider">
+                        <div class="card-header" id="heading0${contsl}">
+                          <h5 class="m-0">
+                            <a class="custom-accordion-title collapsed d-block pt-2 pb-2" data-bs-toggle="collapse"
+                              href="#collapse0${contsl}" aria-expanded="false" aria-controls="collapse0${contsl}">
+                              Imágen de Slider #${contsl} <i class="mdi mdi-close-circle-outline" style="margin-left: 15px;font-size: 20px;color: red;"></i>
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapse0${contsl}" class="collapse" aria-labelledby="heading0${contsl}"
+                          data-bs-parent="#accordionExample">
+                          <div class="card-body">
+
+                            <div class="mb-3">
+                              <div class="row mb-2">
+                                <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                <div class="col-md-10 col-lg-10 col-xl-10 col-xxl-10 col-sm-12">
+                                  <label for="example-fileinput" class="form-label">Cargar Imagen:</label>
+                                  <input type="file" id="example-fileinput0${contsl}" class="form-control imagen">
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                                <div class="col-md-10 col-lg-10 col-xl-10 col-xxl-10 col-sm-12">
+                                  <label for="example-fileinput" class="form-label">Descripción:</label>
+                                  <input type="text" class="form-control mb-2 titulo" id="validationTitulo0${contsl}" placeholder="Título"
+                                    required>
+                                  <input type="text" class="form-control descripcion" id="validationDescrip0${contsl}" placeholder="Descripción"
+                                    required>
+
+                                    <br>
+
+                                    <label for="example-fileinput" class="form-label">Accion:</label>
+
+                                    <div class="mb-2" id='add_input_new_action${contsl}'>
+                                    
+                                      <input class="form-control accioninicial" type="text" placeholder="Formular pregunta" id="textaction0${contsl}" name="textaction0${contsl}" style='margin-bottom: 15px;'>
+
+                                    </div>
+
+                                    <a style="cursor: pointer" onclick='add_new_accion(this)' attr_cont_total='${contsl}' class="text-primary btn_save_action"><i class="mdi mdi-plus-circle me-1"></i>Agregar otra acción</a>
+
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 col-xxl-1 col-sm-12"></div>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>`;
+  contenedorsl.append(sl);
+
+  var sldr = document.querySelector(".btn_save");
+  sldr.setAttribute("attr_cont_slider", contsl);
+
+})
+
+
+
 /*===========================
   VACIAR TODO LOS PRODUCTO DEL LOCASLTORAGE
 =============================*/
@@ -90,7 +232,7 @@ function eliminarLS() {
 /*=============================================
   EVENTO ONCLICK
 =============================================*/
-function myFunction() {
+function myFunction_old() {
   let id = uuidv4()
   let b = document.querySelector(".btn_save");
   let align = b.getAttribute("attr_cont");
@@ -216,6 +358,137 @@ function myFunction() {
     div_rptas.append(rpt)
   }
 }
+
+function myFunction() {
+  let id = uuidv4() //generar un id para cada array
+  let b = document.querySelector(".btn_save");
+  let align = b.getAttribute("attr_cont"); //capturo el atributo en TEXTO que esta inicialmente en cero
+  let old_ques = []
+  let preguntas = [];
+  let est_campos = false;
+  console.log(est_campos)
+
+
+  micont++; //Contador para la vista conforme se va registrando
+  n = -1;
+  n2 = 0;
+  naction = 0;
+  x = 0;
+  x2 = 0;
+
+  //WHILE PARA OBTENER LAS N PREGUNTAS
+  while (n < align) {
+    n++;
+    x = +n;
+    //Id para las preguntas
+    let capId = "question" + x
+    let inputValue = document.getElementById(capId).value //Obtener los values de los input 
+    if (inputValue != '') {
+      est_campos = true
+      old_ques.push(inputValue);
+    } else {
+      est_campos = false
+    }
+  }
+  var seltexto = document.getElementById("customRadio3");
+  var selslider = document.getElementById("customRadio4");
+
+  // SI EL TIPO DE RESPUESTA ES TEXTO
+  if (seltexto.checked == true) {
+    let rpta_i = document.getElementById('txt_autorpta').value
+    preguntas.push({
+      'preguntas_new': old_ques,
+      'respuesta_new': rpta_i,
+      'id': id,
+      'tipo': "texto"
+    })
+    let id_tmp = preguntas[0].id
+    console.log('id_tmp', id_tmp)
+    if (est_campos == false) {
+      alert('Es necesario registrar preguntas')
+    } else if (rpta_i == '') {
+      alert('Es necesario registrar una respuesta')
+    } else {
+      agregarLS(preguntas)
+    }
+  } else if (selslider.checked == true) { // SI EL TIPO DE RESPUESTA ES UN SLIDER
+
+
+    let nslider = document.querySelectorAll('.clsSlider').length; //total de slider imagen
+    let slidersall = document.querySelectorAll('.clsSlider') //trae toda las cabeceras
+    let respuestas = {};
+    let rptaFinal = [];
+
+
+
+
+    slidersall.forEach(sl => {
+      let descripcionsl = sl.querySelector('.descripcion').value;
+      let titulosl = sl.querySelector('.titulo').value;
+      let imgsl = sl.querySelector('.imagen').files[0]['name'];
+      let accioninicial = sl.querySelectorAll('.accioninicial')
+
+      respuestas = {
+        'img': imgsl,
+        'titulo_imagen': titulosl,
+        'descripcion': descripcionsl,
+        'acciones': []
+      }
+
+      rptaFinal.push(respuestas)
+
+      accioninicial.forEach(accion => {
+        respuestas.acciones.push(accion.value);
+      });
+
+    });
+
+
+    preguntas.push({
+      'preguntas_new': old_ques,
+      'respuesta_new': rptaFinal,
+      'id': id,
+      'tipo': 'slider'
+    })
+
+    agregarLS(preguntas)
+
+
+
+
+
+    alert("Slider");
+  } else {
+    alert('no seleccionó')
+  }
+
+}
+
+
+
+/*===========================
+  AGREGAR NUEVA ACCION
+=============================*/
+var conttsld = 0
+
+function add_new_accion(b) {
+  conttsld++;
+  let a = b.getAttribute("attr_cont_total");
+  let contenedor = document.querySelector('#add_input_new_action' + a)
+  document.querySelector('#add_slider').setAttribute('attradd_slider', conttsld) //envio el total de acciones para entrar al while
+  let p = document.createElement('div')
+  p.innerHTML = `
+                <div class="row" style='margin-right: 15px;margin-bottom: 15px;'>
+                  <div class="col-11">
+                    <input class="form-control accioninicial" type="text" placeholder="Formular pregunta" id="textaction${conttsld}" name="textaction${conttsld}">
+                  </div>
+                  <div class="col-1">
+                    <button type="button" id="btn_del" onclick="#" class="btn btn-danger"><i class="mdi mdi-window-close"></i> </button>
+                  </div>
+                </div>`;
+  contenedor.append(p);
+}
+
 /*===========================
 RECUPERAR LAS PREGUNTAS AL LOCAL STORAGE
 =============================*/
@@ -349,14 +622,13 @@ function rpt_aut_save() {
     }).then(function (response) {
       console.log(response);
       eliminarLS()
-      document.getElementById('btn_sav').style.display = 'none'
-      document.getElementById('btn_loader').style.display = 'block'
-      
+      document.getElementById('btn_sav').style.display = 'none';
+      document.getElementById('btn_loader').style.display = 'block';
       // var url_python = '{% url "respuestas" %}?empre_id='+id_empresa
       // location.replace(url_python)
 
-      location.href = "../respuestas/?empre_id="+id_empresa;
-    
+      location.href = "../respuestas/?empre_id=" + id_empresa;
+
     }).catch(e => {
 
 
