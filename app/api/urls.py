765c00bc4b57b2968bec71230a516fb3fdbfe6d0
= <=>
2022-09-28 03:58:00 -0500

    mod reportes actualizado 3

--- app/api/urls.py
+++ app/api/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', login_required(views.ApiOverview), name='home'),
     path('create/', login_required(views.add_items), name='add-items'),
-    path('all/', login_required(views.view_items), name='view_items'),
+    path('all/', views.view_items, name='view_items'),
     path('update/<int:pk>/', login_required(views.update_items), name='update-items'),
     path('item/<int:pk>/delete/', login_required(views.delete_items), name='delete-items'),
 
