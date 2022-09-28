276780c03a458d76a1017afd669ea9c959b9362f
= <=>
2022-09-28 11:11:32 -0500

    cambiando rutas con gunicorn

--- app/bot/settings.py
+++ app/bot/settings.py
@@ -134,8 +134,9 @@ APPEND_SLASH=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 LOGIN_URL = 'login'
 
