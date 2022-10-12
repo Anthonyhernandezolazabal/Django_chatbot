738130737ae654707a251818f3f0dc722ca882dd
root <root@chatbot-app.us-central1-a.c.flash-proxy-359820.internal>
2022-10-12 21:15:10 +0000

    produccion add

--- app/bot/settings.py
+++ app/bot/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-t4z5qn&qcy_#$7ysoqp3$r5%)py4*b)=&43#480rlzkun^27(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ariadna.gq', '34.71.90.164']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
