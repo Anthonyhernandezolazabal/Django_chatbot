e4274c61bbdc8f98e70c75b10838fa16e9c1d6a0
= <=>
2022-11-21 15:55:53 -0500

    cambio en conf2

--- app/bot/settings.py
+++ app/bot/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_WHITELIST = ["*"]
+# CORS_ORIGIN_WHITELIST = ["*"]
 
 ROOT_URLCONF = 'bot.urls'
 
