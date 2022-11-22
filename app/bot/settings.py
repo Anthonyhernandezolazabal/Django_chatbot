983e30babc16ec8ee687cddec354852ba0a07b4c
= <=>
2022-11-22 08:58:12 -0500

    pl

--- app/bot/settings.py
+++ app/bot/settings.py
@@ -17,7 +17,7 @@ DEBUG = False
 # ALLOWED_HOSTS = ['*']
 # CORS_ORIGIN_ALLOW_ALL = False
 
-ALLOWED_HOSTS = ['ariadnachatbot.tk', '34.125.84.198']
+ALLOWED_HOSTS = ['ariadnachatbot.tk', '34.125.108.178']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
